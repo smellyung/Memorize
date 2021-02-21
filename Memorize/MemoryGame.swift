05751688fcf715c1f187667147dd20770ac54f10
Kelly Ung <kelly@entale.co>
2021-02-21 16:31:26 +0000

    update viewModel choose method + add property for faceUp card index

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -3,23 +3,48 @@ import Foundation
 // Model
 // CardContent is dont care type
 // is decided when creating instance of MemoryGame
-struct MemoryGame<CardContent> {
+struct MemoryGame<CardContent> where CardContent: Equatable {
     var cards: Array<Card>
 
-    mutating func choose(card: Card) {
-        print("card chosen: \(card)")
-        if let chosenIndex: Int = self.index(of: card) {
-            self.cards[chosenIndex].isFaceUp = !self.cards[chosenIndex].isFaceUp
+    var indexOfTheOneAndOnlyFaceUpCard: Int? {
+        get {
+            // find all card indices where isFaceUp = true
+            // if there's only 1 card that means its the only face up card
+                // else it is either indices of already matched cards
+                // or it is empty
+            return cards.indices.filter { cards[$0].isFaceUp }.only
+        }
+
+        set {
+            // set corresponding card with index isFaceUp prop to true
+            for index in cards.indices {
+                cards[index].isFaceUp = index == newValue
+            }
         }
     }
 
-    func index(of card: Card) -> Int? {
-        for index in 0..<self.cards.count {
-            if self.cards[index].id == card.id {
-                return index
+    mutating func choose(card: Card) {
+        print("card chosen: \(card)")
+
+        // if you can find the index of the card, it isnt face up nor matched then ..
+        if let chosenIndex: Int = cards.firstIndex(matching: card),
+           !cards[chosenIndex].isFaceUp,
+           !cards[chosenIndex].isMatched {
+
+            // check if it matches the other face up card (if it exist)
+            if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard {
+
+                // check if content matches, if so set the cards isMatched props
+                if cards[chosenIndex].content == cards[potentialMatchIndex].content {
+                    cards[chosenIndex].isMatched = true
+                    cards[potentialMatchIndex].isMatched = true
+                }
+                self.cards[chosenIndex].isFaceUp = true
+            } else {
+                // else no matches so chosen card is the only face up card
+                indexOfTheOneAndOnlyFaceUpCard = chosenIndex
             }
         }
-        return nil
     }
 
     init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
@@ -34,7 +59,7 @@ struct MemoryGame<CardContent> {
 
     struct Card: Identifiable {
         var id: Int
-        var isFaceUp: Bool = true
+        var isFaceUp: Bool = false
         var isMatched: Bool = false
         var content: CardContent
     }
